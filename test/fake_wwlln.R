db4978873fa1f33de6ab31325b5d977c4feb92a6
Michael Hutchins <michael.hutchins@rootmetrics.com>
2015-10-02 13:58:44 -0700

    Start function to generate stroke data.

--- test/fake_wwlln.R
+++ test/fake_wwlln.R
@@ -1,2 +1,34 @@
 ## Replicate WWLLN file generation to perform local end to end tests of the
 # data and map pipelines
+
+if (!require(lubridate)) {
+    install.packages("lubridate")
+}
+
+#' Generate Set of New Strokes
+#' 
+#' Produce a dataframe with typical WWLLN information
+#' 
+#' @export
+#' @param n Number strokes
+newStrokes <- function(n = 1000) {
+
+    timeRange <- 3600
+    
+    ## Replicate chimneys
+    lat <- rnorm(n, 0, 35)
+    lon <- c(rnorm(floor(n/2), 10, 35),
+             rnorm(floor(n/4), -80, 25),
+             rnorm(floor(n/4), 120, 40))
+    
+    ## Cap latitude and wrap longitude (as needed)
+    lat[lat < -89.99] <- -89.99
+    lat[lat > 89.99] <- 89.99
+    lon <- ((lon + 180) %% 360) - 180
+    
+    currentTime <- as.numeric(now())
+    
+    times <- sample(seq(currentTime - timeRange, currentTime), n, replace = TRUE)
+
+
+}
